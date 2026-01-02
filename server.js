5eca2771d67280a1d6c323b1b5f9c4f5377ab9e7
sudarshan sabat <onyourmarkgodigital@gmail.com>
2026-01-02 12:36:31 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,27 +1,24 @@
 require("dotenv").config();
+require("./config/db"); // ✅ IMPORTANT
+
 const express = require("express");
 const cors = require("cors");
 const multer = require("multer");
 
 const companyRoutes = require("./routes/companyRoutes");
 
-
 const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Middleware
 app.use(cors());
-app.use(express.json()); // Parse JSON requests
-app.use(express.urlencoded({ extended: true })); // Parse form data
-const upload = multer(); // For parsing FormData
-// Routes
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
+// Routes
 app.use("/api/companies", companyRoutes);
 
-
-console.log("✅ Database connection pool initialized.");
-
-// Start Server
+// Start server
 app.listen(PORT, () => {
-    console.log(`🚀 Server running on http://localhost:${PORT}`);
+  console.log(`🚀 Server running on http://localhost:${PORT}`);
 });
