f203f0cdebbff029b213d8c8a1f8852756618d3e
sudarshan sabat <onyourmarkgodigital@gmail.com>
2026-01-02 12:06:15 +0530

    Update companyRoutes.js

--- routes/companyRoutes.js
+++ routes/companyRoutes.js
@@ -4,6 +4,14 @@ const upload = require("../middleware/upload");
 const {
   registerCompany,
   loginCompany,
+
+  getAllCompanies,
+  getCompanyById,
+  getCompanyByCode,
+  searchCompanies,
+
+
+
 } = require("../controllers/companyController");
 const { updateCompanyProfile } = require("../controllers/companyController");
 
@@ -11,5 +19,14 @@ router.post("/register", upload.single("photoUrl"), registerCompany);
 router.post("/login", loginCompany);
 
 router.put("/profile/:id", updateCompanyProfile);
+
+
+
+
+/* SHOW / FETCH */
+router.get("/", getAllCompanies);
+router.get("/id/:id", getCompanyById);
+router.get("/code/:code", getCompanyByCode);
+router.get("/search", searchCompanies);
 
 module.exports = router;
