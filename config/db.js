5c65a8c1478904408581292e65ae4ac4a4f66330
sudarshan sabat <onyourmarkgodigital@gmail.com>
2026-01-03 11:23:37 +0530

    Update db.js

--- config/db.js
+++ config/db.js
@@ -1,4 +1,4 @@
-const mysql = require("mysql2");
+const mysql = require("mysql2/promise");
 require("dotenv").config();
 
 const pool = mysql.createPool({
@@ -6,13 +6,13 @@ const pool = mysql.createPool({
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
-  port: 3306,
+  port: process.env.DB_PORT || 3306,
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0,
 });
 
-// Test connection ONCE at startup
+// Test connection once
 (async () => {
   try {
     const conn = await pool.getConnection();
