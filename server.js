e8e25ae66c082fc66aa34b48e1d8a4d22fb9d522
sudarshan sabat <onyourmarkgodigital@gmail.com>
2026-01-09 12:27:15 +0530

    Update server.js

--- server.js
+++ server.js
@@ -39,7 +39,7 @@ app.use(
       "https://bizzmarkindia.com/login",
       "http://srv1235061.hstgr.cloud",
       "http://srv1235061.hstgr.cloud:5000",
-      "http://srv1235061.hstgr.cloud:5000/api/companies/login"
+      "http://srv1235061.hstgr.cloud:5000/api/companies/login",
       "https://srv1235061.hstgr.cloud:5000/api/companies/login"
       
     ],
