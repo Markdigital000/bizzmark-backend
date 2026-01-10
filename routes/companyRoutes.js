747296c3fcc7759e1fdf60d427c630650cabb1c9
sudarshan sabat <onyourmarkgodigital@gmail.com>
2026-01-10 11:48:13 +0530

    Update companyRoutes.js

--- routes/companyRoutes.js
+++ routes/companyRoutes.js
@@ -4,27 +4,12 @@ const upload = require("../middleware/upload");
  
 const {
   registerCompany,
-  loginCompany,
-  getAllCompanies,
-  getCompanyById,
-  getCompanyByCode,
-  searchCompanies,
-  updateCompanyProfile,
+  loginCompany
 } = require("../controllers/companyController");
  
 // AUTH
 router.post("/register", upload.single("photoUrl"), registerCompany);
-
-
 router.post("/login", loginCompany);
- 
-// UPDATE
-router.put("/profile/:id", upload.single("photoUrl"), updateCompanyProfile);
- 
-// FETCH (order matters)
-router.get("/search", searchCompanies);
-router.get("/code/:code", getCompanyByCode);
-router.get("/id/:id", getCompanyById);
-router.get("/", getAllCompanies); // ✅ FIXED: Removed "/api/companies/"
+
  
 module.exports = router;
