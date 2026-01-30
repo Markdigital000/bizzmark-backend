d3605c77f6d704d7772d13d19f0873c6558903ea
sudarshan sabat <onyourmarkgodigital@gmail.com>
2026-01-30 10:56:17 +0530

    Update upload.js

--- middleware/upload.js
+++ middleware/upload.js
@@ -8,24 +8,4 @@ const storage = multer.diskStorage({
   },
 });
 
-const fileFilter = (req, file, cb) => {
-  const allowedTypes = /jpeg|jpg|png|webp/;
-  const ext = allowedTypes.test(
-    path.extname(file.originalname).toLowerCase()
-  );
-  const mime = allowedTypes.test(file.mimetype);
-
-  if (ext && mime) {
-    cb(null, true);
-  } else {
-    cb(new Error("Only image files are allowed"), false);
-  }
-};
-
-module.exports = multer({
-  storage,
-  limits: {
-    fileSize: 10 * 1024 * 1024, // ✅ 10 MB
-  },
-  fileFilter,
-});
+module.exports = multer({ storage });
