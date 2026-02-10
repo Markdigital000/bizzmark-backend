92330b3d40c3886a949dee175566f9ff251af757
sudarshan sabat <onyourmarkgodigital@gmail.com>
2026-02-10 16:34:38 +0530

    Update authRoutes.js

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,8 +1,8 @@
 const express = require("express");
 const router = express.Router();
-const authController = require("../controllers/authController");
+const { sendOtp, verifyOtp } = require("../controllers/authController");
 
-router.post("/send-otp", authController.sendOtp);
-router.post("/verify-otp", authController.verifyOtp);
+router.post("/send-otp", sendOtp);
+router.post("/verify-otp", verifyOtp);
 
 module.exports = router;
