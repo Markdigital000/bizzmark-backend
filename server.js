0eadf9bc28077d8e9804543f2c1d9e5a1a76654e
sudarshan sabat <onyourmarkgodigital@gmail.com>
2026-02-10 16:36:07 +0530

    Update server.js

--- server.js
+++ server.js
@@ -40,10 +40,12 @@ app.use(
       "http://srv1235061.hstgr.cloud",
       "http://localhost:3000"
     ],
+   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
     credentials: true,
   })
 );
- 
+ app.use(express.json());
 app.use(express.json({ limit: "25mb" }));
 app.use(express.urlencoded({ extended: true, limit: "25mb" }));
 
